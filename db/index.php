d3872eba5334006ad46b53314e32a816c2b60cfc
lisa <lisa.casaburo@outlook.it>
2026-01-12 11:08:48 +0100

    db

---
+++ db/index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Il testo che segue arriva dal database</h1>
+    <div>SELECT * FROM `content`;</div>
+    
+</body>
+</html>
