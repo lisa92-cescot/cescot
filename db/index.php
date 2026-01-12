3e6482a55dfe4eeca10add9d175cfb1b8e7cc9c9
lisa <lisa.casaburo@outlook.it>
2026-01-12 11:13:32 +0100

    butta

--- db/index.php
+++ db/index.php
@@ -6,7 +6,32 @@
 </head>
 <body>
     <h1>Il testo che segue arriva dal database</h1>
-    <div>SELECT * FROM `content`;</div>
+    <div><?php
+//inizializza la connessione al database
+$databaseHost = 'localhost';
+$databaseName = 'prenotazioni';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+//verifica la connessione
+if (!$mysqli) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+//esegui una query di esempio
+$query = 'SELECT * FROM clienti';
+
+$result = mysqli_query($mysqli, $query);
+
+//stampa il numero di righe restituite
+echo $result->num_rows . '<br>';
+
+//ciclo sulle righe restituite e stampo nome e cognome di ogni cliente
+while ($row = mysqli_fetch_assoc($result)) {
+	echo 'Nome: ' . $row['nome'] . ', Cognome: ' . $row['cognome'] . '<br>';
+}</div>
     
 </body>
 </html>
